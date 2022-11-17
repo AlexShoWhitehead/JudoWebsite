54e9969bfe05f9493c1e162f6069b9e84f547e5d
ALEX <asw236@cornell.edu>
2022-11-16 21:51:39 -0500

    added accordion id's to about-team.html

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,20 +1,100 @@
 // Source: (original work) Kyle Harms Snippets
-// Accordion JS
-$("#accordion").click(function() {
-    if ($("#dropd").hasClass("hidden")) {
-        $("#dropd").removeClass("hidden");
+// Accordion One JS
+$("#accordion_one").click(function() {
+    if ($("#dropd_one").hasClass("hide")) {
+        $("#dropd_one").removeClass("hide");
     }
     else {
-        $("#dropd").addClass("hidden");
+        $("#dropd_one").addClass("hide");
     }
 })
 
-$("#accordion").click(function() {
-    if ($("#accordion").hasClass("active")) {
-        $("#accordion").removeClass("active");
+$("#accordion_one").click(function() {
+    if ($("#accordion_one").hasClass("active")) {
+        $("#accordion_one").removeClass("active");
     }
     else {
-        $("#accordion").addClass("active");
+        $("#accordion_one").addClass("active");
     }
 })
-// Accordion JS
+// Accordion One JS
+
+// Accordion Two JS
+$("#accordion_two").click(function() {
+    if ($("#dropd_two").hasClass("hide")) {
+        $("#dropd_two").removeClass("hide");
+    }
+    else {
+        $("#dropd_two").addClass("hide");
+    }
+})
+
+$("#accordion_two").click(function() {
+    if ($("#accordion_two").hasClass("active")) {
+        $("#accordion_two").removeClass("active");
+    }
+    else {
+        $("#accordion_two").addClass("active");
+    }
+})
+// Accordion Two JS
+
+// Accordion Three JS
+$("#accordion_three").click(function() {
+    if ($("#dropd_three").hasClass("hide")) {
+        $("#dropd_three").removeClass("hide");
+    }
+    else {
+        $("#dropd_three").addClass("hide");
+    }
+})
+
+$("#accordion_three").click(function() {
+    if ($("#accordion_three").hasClass("active")) {
+        $("#accordion_three").removeClass("active");
+    }
+    else {
+        $("#accordion_three").addClass("active");
+    }
+})
+// Accordion Three JS
+
+// Accordion Four JS
+$("#accordion_four").click(function() {
+    if ($("#dropd_four").hasClass("hide")) {
+        $("#dropd_four").removeClass("hide");
+    }
+    else {
+        $("#dropd_four").addClass("hide");
+    }
+})
+
+$("#accordion_four").click(function() {
+    if ($("#accordion_four").hasClass("active")) {
+        $("#accordion_four").removeClass("active");
+    }
+    else {
+        $("#accordion_four").addClass("active");
+    }
+})
+// Accordion Four JS
+
+// Accordion Five JS
+$("#accordion_five").click(function() {
+    if ($("#dropd_five").hasClass("hide")) {
+        $("#dropd_dive").removeClass("hide");
+    }
+    else {
+        $("#dropd_five").addClass("hide");
+    }
+})
+
+$("#accordion_five").click(function() {
+    if ($("#accordion_five").hasClass("active")) {
+        $("#accordion_five").removeClass("active");
+    }
+    else {
+        $("#accordion_five").addClass("active");
+    }
+})
+// Accordion Five JS
